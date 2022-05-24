cd54fc70872a35644ac275fa26d2628c97b6cb2a
Piotr162991 <100698056+Piotr162991@users.noreply.github.com>
2022-05-24 13:30:10 +0000

    6_2 działa w połowie

--- script.js
+++ script.js
@@ -2,4 +2,34 @@
   ex1_button.addEventListener("click",function(){
     ex1_content.innerHTML = "1 2 3 4 5 6 7 8 9"
   })
+    
+        ex2_text.querySelector("phone number")
+        
+        ex2_text.addEventListener('input', function (){
+          if(ex2_text.lenght==9)
+          
+          {
+            ex2_content.innerHTML = "dobry numer";
+          }
+          else{
+            ex2_content.innerHTML = "zły numer";
+          }
+        });
+  /*ex2_text.addEventListener("text",s)
+  
+  function s()
+  {
+    ex2_text.InputEvent("",function(){
+      if(ex2_text.lenght==9)
+    {
+      ex2_content.innerHTML = "dobry numer"
+    }
+    else {
+      ex2_content.innerHTML = "zły numer"
+    }
+    })
+    
+      
+    
+  }*/
 })();
